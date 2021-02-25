4de59b8df067f7f8c2e559866bfb30f46454eca5
harrygillingham99 <harrygillingham99@gmail.com>
2021-02-25 17:50:15 +0000

    feat(GraphQL example): figured out the HORRIBLE argument syntax for OctoKit GraphQL, consolidate packages

--- RepoAnalyser.Objects/Constants/GitHubConstants.cs
+++ RepoAnalyser.Objects/Constants/GitHubConstants.cs
@@ -1,7 +1,11 @@
-﻿namespace RepoAnalyser.Objects.Constants
+﻿using System.Collections.Generic;
+using Octokit.GraphQL.Core;
+using Octokit.GraphQL.Model;
+
+namespace RepoAnalyser.Objects.Constants
 {
     public static class GitHubConstants
     {
-
+        public static Arg<IEnumerable<RepositoryAffiliation?>> RepositoryScopes = new Arg<IEnumerable<RepositoryAffiliation?>>(new List<RepositoryAffiliation?> { RepositoryAffiliation.Owner, RepositoryAffiliation.Collaborator });
     }
 }
--- RepoAnalyser.OctoKit/OctoKit.GraphQL/OctoKitGraphQLServiceAgent.cs
+++ RepoAnalyser.OctoKit/OctoKit.GraphQL/OctoKitGraphQLServiceAgent.cs
@@ -2,8 +2,11 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Octokit.GraphQL;
+using Octokit.GraphQL.Core;
+using Octokit.GraphQL.Model;
 using RepoAnalyser.Objects;
 using RepoAnalyser.Services.OctoKit.GraphQL.Interfaces;
+using static RepoAnalyser.Objects.Constants.GitHubConstants;
 
 namespace RepoAnalyser.Services.OctoKit.GraphQL
 {
@@ -18,21 +21,21 @@ namespace RepoAnalyser.Services.OctoKit.GraphQL
 
         public Task<IEnumerable<Repo>> GetRepositories(string token)
         {
-            var query = new Query().Viewer.Repositories().AllPages()
+            var query = new Query().Viewer
+                .Repositories(100, affiliations: RepositoryScopes).Nodes
                 .Select(x => new Repo
                 {
                     Description = x.Description, Name = x.Name
-
                 }).Compile();
-            return BuildConnectionAndQuery(token, query);
-
+            return BuildConnectionExecuteQuery(token, query);
         }
 
-        private Task<T> BuildConnectionAndQuery<T>(string token, ICompiledQuery<T> query)
+        private Task<T> BuildConnectionExecuteQuery<T>(string token, ICompiledQuery<T> query,
+            Dictionary<string, object> variables = null)
         {
             var connection = new Connection(_productHeaderValue, token);
-            return connection.Run(query);
-        } 
+            return connection.Run(query, variables);
+        }
     }
 
     public class Repo
