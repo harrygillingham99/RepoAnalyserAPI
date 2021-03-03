76a774971366ce2d4cd84befb1d85885d7ae816e
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-03 14:31:31 +0000

    fix: logging in repository base

--- RepoAnalyser.SqlServer.DAL/BaseRepository/Repository.cs
+++ RepoAnalyser.SqlServer.DAL/BaseRepository/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Serilog;
 
@@ -9,14 +10,17 @@ namespace RepoAnalyser.SqlServer.DAL.BaseRepository
     public abstract class Repository
     {
         private readonly string _connectionString;
+        private readonly Stopwatch _stopwatch;
 
         protected Repository(string connectionString)
         {
             _connectionString = connectionString;
+            _stopwatch = new Stopwatch();
         }
 
         protected async Task<T> Invoke<T>(Func<IDbConnection, Task<T>> getData)
         {
+            _stopwatch.Start();
             try
             {
                 using var connection = new SqlConnection(_connectionString);
@@ -29,10 +33,16 @@ namespace RepoAnalyser.SqlServer.DAL.BaseRepository
                 Log.Error(ex, exceptionMsg);
                 throw new Exception(exceptionMsg, ex);
             }
+            finally
+            {
+                _stopwatch.Stop();
+                Debug.WriteLine($"SQL Operation completed in {_stopwatch.ElapsedMilliseconds}");
+            }
         }
 
-        protected async Task InvokeWithTransaction<T>(Func<IDbConnection, Task<T>> dbOperation)
+        protected async Task InvokeWithTransaction<T>(Func<IDbConnection, Task<T>> dbOperation, bool rollbackOnFault = true)
         {
+            _stopwatch.Start();
             using var connection = new SqlConnection(_connectionString);
             await connection.OpenAsync();
             using var tran = connection.BeginTransaction();
@@ -43,10 +53,22 @@ namespace RepoAnalyser.SqlServer.DAL.BaseRepository
             }
             catch (Exception ex)
             {
-                tran.Rollback();
-                var exceptionMsg = $"{GetType().FullName}.Invoke experienced a {ex.GetType()}";
+                if (rollbackOnFault)
+                {
+                    tran.Rollback();
+                }
+                else
+                {
+                    Log.Information(ex, $"Rollback skipped after exception for {dbOperation.GetType()}");
+                }
+                var exceptionMsg = $"{GetType().FullName}.InvokeWithTransaction experienced a {ex.GetType()}";
                 Log.Error(ex, exceptionMsg);
                 throw new Exception(exceptionMsg, ex);
+            }
+            finally
+            {
+                _stopwatch.Stop();
+                Debug.WriteLine($"SQL Transaction Operation completed in {_stopwatch.ElapsedMilliseconds}");
             }
         }
     }
