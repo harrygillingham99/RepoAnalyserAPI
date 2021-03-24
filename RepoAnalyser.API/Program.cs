5d9974b133b841807c95aea469f0865872294ac2
harrygillingham99 <harrygillingham99@gmail.com>
2021-03-24 16:44:40 +0000

    chore(packages): update packages + enrich serilog logs

--- RepoAnalyser.API/Program.cs
+++ RepoAnalyser.API/Program.cs
@@ -15,6 +15,9 @@ namespace RepoAnalyser.API
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .Enrich.FromLogContext()
+                .Enrich.WithEnvironmentUserName()
+                .Enrich.WithMachineName()
+                .Enrich.WithMemoryUsage()
                 .WriteTo.Logger(lc => lc
                     .Filter.ByIncludingOnly(ev => ev.Level == LogEventLevel.Error || ev.Level == LogEventLevel.Fatal)
                     .WriteTo.File(
@@ -23,7 +26,8 @@ namespace RepoAnalyser.API
                         shared: true,
                         fileSizeLimitBytes: 20_971_520, // roughly 20 MB
                         rollOnFileSizeLimit: true,
-                        retainedFileCountLimit: 10))
+                        retainedFileCountLimit: 10)
+                    .WriteTo.Console())
                 .WriteTo.Logger(lc => lc
                     .Filter.ByIncludingOnly(ev => ev.Level == LogEventLevel.Information || ev.Level == LogEventLevel.Warning)
                     .WriteTo.File(
@@ -32,7 +36,8 @@ namespace RepoAnalyser.API
                         shared: true,
                         fileSizeLimitBytes: 20_971_520, // roughly 20 MB
                         rollOnFileSizeLimit: true,
-                        retainedFileCountLimit: 10))
+                        retainedFileCountLimit: 10)
+                    .WriteTo.Console())
                 .CreateLogger();
 
             try
