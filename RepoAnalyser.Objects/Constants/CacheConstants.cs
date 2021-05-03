0d6ec97ca73e066c5ec2a0404d4e00989da105ad
harrygillingham99 <harrygillingham99@gmail.com>
2021-05-03 11:08:39 +0100

    fix: make cache expiry shorter for testing

--- RepoAnalyser.Objects/Constants/CacheConstants.cs
+++ RepoAnalyser.Objects/Constants/CacheConstants.cs
@@ -5,6 +5,8 @@ namespace RepoAnalyser.Objects.Constants
     public static class CacheConstants
     {
         public static DateTimeOffset DefaultCacheExpiry = DateTimeOffset.Now.AddHours(1);
-        public static TimeSpan DefaultSlidingCacheExpiry = new TimeSpan(0, 5, 0);
+
+        //set it low for testing
+        public static TimeSpan DefaultSlidingCacheExpiry = new TimeSpan(0, 1, 0);
     }
 }
